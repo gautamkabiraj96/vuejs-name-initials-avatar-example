1d28f22916bdb0ced2cc1bf8f972255fc1a457f2
Gautam Kabiraj <51134142+gautamkabiraj96@users.noreply.github.com>
2022-10-21 01:14:15 +0530

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# vuejs-name-initials-avatar-example
-Created with CodeSandbox
+# Avatar initials for names Vue.js example
+
+Learn how to create an avatar with names quickly in Vue.js with library and customize the library as needed. [Learn more](https://www.nightprogrammer.com/vue-js/how-to-add-an-avatar-in-vue-js-quickly/)
---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="app">
+    <div class="avatar-container">
+      <avatar inline username="Gautam Kabiraj"></avatar>Gautam Kabiraj
+    </div>
+    <div class="avatar-container">
+      <avatar inline username="Tom Smith"></avatar>Tom Smith
+    </div>
+    <div class="avatar-container">
+      <avatar inline username="Lucy Frank"></avatar>Lucy Frank
+    </div>
+    <div class="avatar-container">
+      <avatar inline username="Mona Lisa"></avatar>Mona Lisa
+    </div>
+  </div>
+</template>
+
+<script>
+import Avatar from "vue-avatar";
+
+export default {
+  name: "App",
+  components: {
+    Avatar,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #000000;
+  margin-top: 60px;
+}
+.avatar-container {
+  padding: 8px;
+}
+</style>
